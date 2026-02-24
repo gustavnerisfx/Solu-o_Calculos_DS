be8f87fa2300773cbbb7ca88716199a7adb78801
neris <neris@LENOVO>
2026-02-24 19:01:55 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_calculo_soma/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace projeto_calculo_soma
+{
+    partial class CalculadoraSoma
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalculadoraSoma));
+            this.textnum1 = new System.Windows.Forms.TextBox();
+            this.textnum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textnum1
+            // 
+            this.textnum1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textnum1.Location = new System.Drawing.Point(161, 113);
+            this.textnum1.Name = "textnum1";
+            this.textnum1.Size = new System.Drawing.Size(125, 22);
+            this.textnum1.TabIndex = 0;
+            this.textnum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textnum1.TextChanged += new System.EventHandler(this.textnum1_TextChanged);
+            // 
+            // textnum2
+            // 
+            this.textnum2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textnum2.Location = new System.Drawing.Point(161, 151);
+            this.textnum2.Name = "textnum2";
+            this.textnum2.Size = new System.Drawing.Size(125, 22);
+            this.textnum2.TabIndex = 1;
+            this.textnum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textnum2.TextChanged += new System.EventHandler(this.textnum2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(118, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "_______________________";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(100, 263);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(181, 263);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(262, 263);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(149, 205);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(146, 41);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblResultado.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // CalculadoraSoma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 552);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textnum2);
+            this.Controls.Add(this.textnum1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CalculadoraSoma";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CalculadoraSoma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textnum1;
+        private System.Windows.Forms.TextBox textnum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ projeto_calculo_soma/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_calculo_soma
+{
+    public partial class CalculadoraSoma : Form
+    {
+        public CalculadoraSoma()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculadoraSoma_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textnum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textnum2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textnum1.Text);
+            double b = double.Parse(textnum2.Text);
+            label1.Text = "+";
+            lblResultado.Text = (a + b).ToString();
+
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            label1.Text = "?";
+            lblResultado.Text = "?";
+            textnum1.Clear();
+            textnum2.Clear();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
