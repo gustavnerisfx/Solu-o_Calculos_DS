44df8c6203dbf2338c69d30816ed0e9d3fd2a1b2
gustavnerisfx <gustavonerissilva0@gmail.com>
2026-03-02 11:39:20 -0300

    continuação da aula.

--- projeto_calculo_soma/Form1.Designer.cs
+++ projeto_calculo_soma/Form1.Designer.cs
@@ -38,14 +38,20 @@
             this.btnFechar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnComparar = new System.Windows.Forms.Button();
+            this.btnParImpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textnum1
             // 
             this.textnum1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textnum1.Location = new System.Drawing.Point(161, 113);
+            this.textnum1.Location = new System.Drawing.Point(65, 37);
+            this.textnum1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textnum1.Name = "textnum1";
-            this.textnum1.Size = new System.Drawing.Size(125, 22);
+            this.textnum1.Size = new System.Drawing.Size(178, 26);
             this.textnum1.TabIndex = 0;
             this.textnum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textnum1.TextChanged += new System.EventHandler(this.textnum1_TextChanged);
@@ -53,9 +59,10 @@
             // textnum2
             // 
             this.textnum2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textnum2.Location = new System.Drawing.Point(161, 151);
+            this.textnum2.Location = new System.Drawing.Point(65, 82);
+            this.textnum2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textnum2.Name = "textnum2";
-            this.textnum2.Size = new System.Drawing.Size(125, 22);
+            this.textnum2.Size = new System.Drawing.Size(178, 26);
             this.textnum2.TabIndex = 1;
             this.textnum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textnum2.TextChanged += new System.EventHandler(this.textnum2_TextChanged);
@@ -65,9 +72,9 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(118, 136);
+            this.label1.Location = new System.Drawing.Point(31, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 18);
+            this.label1.Size = new System.Drawing.Size(20, 22);
             this.label1.TabIndex = 2;
             this.label1.Text = "?";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -75,9 +82,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(137, 176);
+            this.label2.Location = new System.Drawing.Point(27, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.Size = new System.Drawing.Size(216, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "_______________________";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -85,9 +92,10 @@
             // btnSomar
             // 
             this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSomar.Location = new System.Drawing.Point(100, 263);
+            this.btnSomar.Location = new System.Drawing.Point(296, 34);
+            this.btnSomar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSomar.Name = "btnSomar";
-            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.Size = new System.Drawing.Size(125, 50);
             this.btnSomar.TabIndex = 4;
             this.btnSomar.Text = "Somar";
             this.btnSomar.UseVisualStyleBackColor = true;
@@ -96,9 +104,10 @@
             // btnLimpar
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(181, 263);
+            this.btnLimpar.Location = new System.Drawing.Point(31, 296);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.Size = new System.Drawing.Size(84, 29);
             this.btnLimpar.TabIndex = 5;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -107,9 +116,10 @@
             // btnFechar
             // 
             this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFechar.Location = new System.Drawing.Point(262, 263);
+            this.btnFechar.Location = new System.Drawing.Point(337, 296);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.Size = new System.Drawing.Size(84, 29);
             this.btnFechar.TabIndex = 6;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = true;
@@ -120,25 +130,90 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(0, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(51, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "label3";
             // 
             // lblResultado
             // 
             this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblResultado.Location = new System.Drawing.Point(140, 206);
+            this.lblResultado.Location = new System.Drawing.Point(31, 143);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(146, 41);
+            this.lblResultado.Size = new System.Drawing.Size(212, 60);
             this.lblResultado.TabIndex = 8;
             this.lblResultado.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.lblResultado.Click += new System.EventHandler(this.label4_Click);
             // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(296, 92);
+            this.btnSubtrair.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(125, 53);
+            this.btnSubtrair.TabIndex = 9;
+            this.btnSubtrair.Text = "&Subtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(296, 153);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(125, 46);
+            this.btnDividir.TabIndex = 10;
+            this.btnDividir.Text = "&Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMulti.Location = new System.Drawing.Point(296, 207);
+            this.btnMulti.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(125, 54);
+            this.btnMulti.TabIndex = 11;
+            this.btnMulti.Text = "&Multiplicar";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnComparar
+            // 
+            this.btnComparar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComparar.Location = new System.Drawing.Point(31, 211);
+            this.btnComparar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnComparar.Name = "btnComparar";
+            this.btnComparar.Size = new System.Drawing.Size(101, 54);
+            this.btnComparar.TabIndex = 12;
+            this.btnComparar.Text = "Comparar";
+            this.btnComparar.UseVisualStyleBackColor = true;
+            this.btnComparar.Click += new System.EventHandler(this.btnComparar_Click);
+            // 
+            // btnParImpar
+            // 
+            this.btnParImpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParImpar.Location = new System.Drawing.Point(142, 211);
+            this.btnParImpar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnParImpar.Name = "btnParImpar";
+            this.btnParImpar.Size = new System.Drawing.Size(101, 54);
+            this.btnParImpar.TabIndex = 13;
+            this.btnParImpar.Text = "Par Ímpar";
+            this.btnParImpar.UseVisualStyleBackColor = true;
+            this.btnParImpar.Click += new System.EventHandler(this.btnParImpar_Click);
+            // 
             // CalculadoraSoma
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 552);
+            this.ClientSize = new System.Drawing.Size(450, 417);
+            this.Controls.Add(this.btnParImpar);
+            this.Controls.Add(this.btnComparar);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnSubtrair);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnFechar);
@@ -149,6 +224,7 @@
             this.Controls.Add(this.textnum2);
             this.Controls.Add(this.textnum1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "CalculadoraSoma";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.CalculadoraSoma_Load);
@@ -168,6 +244,11 @@
         private System.Windows.Forms.Button btnFechar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnComparar;
+        private System.Windows.Forms.Button btnParImpar;
     }
 }
 
--- projeto_calculo_soma/Form1.cs
+++ projeto_calculo_soma/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -39,11 +40,19 @@ namespace projeto_calculo_soma
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(textnum1.Text);
-            double b = double.Parse(textnum2.Text);
+            
             label1.Text = "+";
-            lblResultado.Text = (a + b).ToString();
-
+            try
+            {
+                double a = double.Parse(textnum1.Text);
+                double b = double.Parse(textnum2.Text);
+                lblResultado.Text = (a + b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira números válidos.");
+            }
+            
 
         }
 
@@ -68,6 +77,76 @@ namespace projeto_calculo_soma
         private void label4_Click(object sender, EventArgs e)
         {
             
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "/";
+            try
+            {
+                double a = double.Parse(textnum1.Text);
+                double b = double.Parse(textnum2.Text);
+                lblResultado.Text = (a / b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira números válidos.");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Não é possível dividir por zero.");
+            }
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            label1.Text = "-";
+            try
+            {
+                double a = double.Parse(textnum1.Text);
+                double b = double.Parse(textnum2.Text);
+                lblResultado.Text = (a - b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira números válidos.");
+            }
+            
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            label1.Text = "*";
+            try
+            {
+                double a = double.Parse(textnum1.Text);
+                double b = double.Parse(textnum2.Text);
+                lblResultado.Text = (a * b).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira números válidos.");
+            }
+            
+        }
+
+        private void btnParImpar_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textnum1.Text);
+            double b = double.Parse(textnum2.Text);
+            if (a % 2 == 0)
+            {
+                MessageBox.Show($"{a} é par.");
+            }
+            else
+            {
+                MessageBox.Show($"{a} é ímpar.");
+            }
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
 
         }
     }
