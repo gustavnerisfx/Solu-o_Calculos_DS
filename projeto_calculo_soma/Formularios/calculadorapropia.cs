867f88fb4b9ce65d58116390c5e2fbd02dc3d695
gustavnerisfx <gustavonerissilva0@gmail.com>
2026-03-31 21:22:02 -0300

    adiconei mais funcionalidades na calculadora autoral

--- projeto_calculo_soma/Formularios/calculadorapropia.cs
+++ projeto_calculo_soma/Formularios/calculadorapropia.cs
@@ -42,7 +42,6 @@ namespace projeto_calculo_soma.Formularios
             lblHistorico.Text = "";
             lblHistorico.Text += vNumAnt + " " + vOperacao + " ";
             this.ActiveControl = null;
-
         }
 
 
@@ -58,9 +57,155 @@ namespace projeto_calculo_soma.Formularios
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void btnVirgula_Click(object sender, EventArgs e)
+        {
+            if(!lblVisor.Text.Contains(","))
+            {
+                lblVisor.Text += ",";
+            }
+            this.ActiveControl = null;
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            vNumAnt = 0;
+            lblVisor.Text = "0";
+            lblHistorico.Text = "";
+            this.ActiveControl = null;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            lblVisor.Text = lblVisor.Text.Substring(0, lblVisor.Text.Length - 1);
+            if(lblVisor.Text == "0")
+            {
+                lblVisor.Text = "";
+            }
+            this.ActiveControl = null;
+        }
+
+        private void btnMM_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(lblVisor.Text, out double valor))
+            {
+                valor *= -1;
+                lblVisor.Text = valor.ToString();
+            }
+            this.ActiveControl = null;
+        }
+
+        private void calculadorapropia_KeyDown(object sender, KeyEventArgs e)
+        {
+            label1.Text = e.KeyCode.ToString();
+            Button botao = new Button();
+
+            if(e.KeyCode == Keys.Back)
+            {
+                lblVisor.Text = lblVisor.Text.Substring(0, lblVisor.Text.Length - 1);
+                if (lblVisor.Text == "0")
+                {
+                    lblVisor.Text = "";
+                }
+                this.ActiveControl = null;
+            }
+
+            if(e.KeyCode == Keys.Enter)
+            {
+                decimal vNumAtual = decimal.Parse(lblVisor.Text);
+
+                switch (vOperacao)
+                {
+                    case "+":
+                        lblVisor.Text = (vNumAnt + vNumAtual).ToString();
+                        break;
+                    case "-":
+                        lblVisor.Text = (vNumAnt - vNumAtual).ToString();
+                        break;
+                    case "*":
+                        lblVisor.Text = (vNumAnt * vNumAtual).ToString();
+                        break;
+                    case "/":
+                        lblVisor.Text = (vNumAnt / vNumAtual).ToString();
+                        break;
+                    case "^":
+                        lblVisor.Text = (Math.Pow((double)vNumAnt, (double)vNumAtual)).ToString();
+                        break;
+                }
+                lblHistorico.Text += vNumAtual + " = ";
+                this.ActiveControl = null;
+            }
+            
+            if (e.KeyCode == Keys.Escape) 
+            {
+                Close();
+            }
+            if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                botao.Text = e.KeyCode.ToString().Substring(1);
+                f_digitos(botao, e);
+            }
+            if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+            {
+                botao.Text = e.KeyCode.ToString().Substring(6);
+                f_digitos(botao, e);
+            }
+
+            switch(e.KeyCode)
+            {
+                case Keys.Add:
+                    botao.Text = "+";
+                    f_operacoes(botao, e);
+                    break;
+                case Keys.Subtract:
+                    botao.Text = "-";
+                    f_operacoes(botao, e);
+                    break;
+                case Keys.Divide:
+                    botao.Text = "/";
+                    f_operacoes(botao, e);
+                    break;
+                case Keys.Multiply:
+                    botao.Text = "*";
+                    f_operacoes(botao, e);
+                    break;
+            }
+
+            this.KeyPreview = true;
+        }
+
+        private void calculadorapropia_Shown(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void btnIgual_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            if (vNumAnt != 0)
+            {
+                lblVisor.Text = "";
+                
+            }
+            else
+            {
+                lblVisor.Text = "";
+                vNumAnt = 0;
+            }
+            this.ActiveControl = null;
+        }
+
+        private void btn_iGual_Click(object sender, EventArgs e)
+        {
             decimal vNumAtual = decimal.Parse(lblVisor.Text);
 
-            switch(vOperacao)
+            switch (vOperacao)
             {
                 case "+":
                     lblVisor.Text = (vNumAnt + vNumAtual).ToString();
@@ -79,56 +224,7 @@ namespace projeto_calculo_soma.Formularios
                     break;
             }
             lblHistorico.Text += vNumAtual + " = ";
-        }
-
-        private void btnVirgula_Click(object sender, EventArgs e)
-        {
-            if(!lblVisor.Text.Contains(","))
-            {
-                lblVisor.Text += ",";
-            }
-            
-        }
-
-        private void btnC_Click(object sender, EventArgs e)
-        {
-            vNumAnt = 0;
-            lblVisor.Text = "0";
-            lblHistorico.Text = "";
-            
-        }
-
-        private void btnBack_Click(object sender, EventArgs e)
-        {
-            lblVisor.Text = lblVisor.Text.Substring(0, lblVisor.Text.Length - 1);
-            if(lblVisor.Text == "0")
-            {
-                lblVisor.Text = "";
-            }
-        }
-
-        private void btnMM_Click(object sender, EventArgs e)
-        {
-            if (double.TryParse(lblVisor.Text, out double valor))
-            {
-                valor *= -1;
-                lblVisor.Text = valor.ToString();
-            }
-        }
-
-        private void calculadorapropia_KeyDown(object sender, KeyEventArgs e)
-        {
-            label1.Text = e.KeyCode.ToString();
-            Button botao = new Button();
-            if (e.KeyCode == Keys.Escape) 
-            {
-                Close();
-            }
-            if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
-            {
-                botao.Text = e.KeyCode.ToString().Substring(6);
-                f_digitos(botao, e);
-            }
+            this.ActiveControl = null;
         }
     }
 }
